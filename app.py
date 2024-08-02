79ce238ed2ee2d47623100723030ed6fc6efe9ff
AndySiul26 <125928966+AndySiul26@users.noreply.github.com>
2024-08-02 05:56:41 -0600

    END_POINTS_TESTED

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 
 # Configuración de Supabase
 supabase: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
+
+@app.route('/insert_servicio', methods=['POST'])
+def insert_servicio():
+    try:
+        data = request.json
+        response = supabase.table("servicios").insert(data).execute()
+        if response.data:
+            return jsonify(response.data[0]), 201
+        else:
+            return jsonify({"error": response.error}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 @app.route('/insert_cliente', methods=['POST'])
 def insert_cliente():
     try:
@@ -59,11 +72,25 @@ def insert_sitio_web_negocio():
 def insert_venta():
     try:
         data = request.json
+        
+        # Obtener los detalles del servicio
+        servicio = supabase.table("servicios").select("*").eq("id", data['id_servicio']).execute()
+        if not servicio.data:
+            return jsonify({"error": "Servicio no encontrado"}), 404
+        
+        servicio = servicio.data[0]
+        
+        # Calcular el precio total del servicio
+        precio_total_servicio = (servicio['precio_normal'] - servicio['descuento']) * data['cantidad']
+        
+        # Añadir el precio_total_servicio a los datos
+        data['precio_total_servicio'] = precio_total_servicio
+        
         response = supabase.table("ventas").insert(data).execute()
-        if response.data == 201:
+        if response.data:
             return jsonify(response.data[0]), 201
         else:
-            return jsonify({"error": response.error_message}), 500
+            return jsonify({"error": response.error}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -71,12 +98,23 @@ def insert_venta():
 def insert_comprobante():
     try:
         data = request.json
+        # Comprobar que sea un texto concatenado por comas ids_ventas o directamente un entero
+        ids_ventas_list = [0]
+        
+        if (type(data.get("ids_ventas")) != str and type(data.get("ids_ventas")) == int):
+            ids_ventas_list[0] = data.get("ids_ventas")
+        else:
+            ids_ventas_list = data.get("ids_ventas").split(",") # Convertir la cadena en una lista de enteros
+        
+        del data["ids_ventas"]  # Eliminar la propiedad "ids_ventas" del diccionario "data"
+        
         response = supabase.table("comprobantes").insert(data).execute()
+        
         if response.data:
             comprobante_id = response.data[0]["id"]
-
+            
             # Insertar en ventas_comprobantes
-            ids_ventas_list = data.get("ids_ventas").split(",")
+            
             for id_venta in ids_ventas_list:
                 insert_data = {
                     "id_comprobante": comprobante_id,
